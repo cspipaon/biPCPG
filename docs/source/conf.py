e0929b94fa8c8baa19bb6c8fe8d60770bc2eb02c
pipaon <cspipaon@gmail.com>
2021-09-27 14:31:56 +0200

    Added sphinx extensions to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,12 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
